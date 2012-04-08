c8f98b96eda08a36ddf3c8ddf75082b67cc8fa00
david <david@mediaodyssee.com>
2012-04-08 19:46:43 +0200

    update dependancies script

--- install_dependancies.sh
+++ install_dependancies.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-aptitude install python python-django python-pip dmtcp python-zmq
+aptitude install python python-django python-pip dmtcp python-zmq bsdgames
 pip install south
 pip install django-tastypie
 pip install pexpect
 pip install python-daemon
+pip install psutil
 
 
