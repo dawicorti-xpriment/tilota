e75cb657ffc84a1fd7a8e49d175676cea93f7855
david <david@mediaodyssee.com>
2012-04-11 21:50:19 +0200

    utils logger

--- core/utils.py
+++ core/utils.py
@@ -3,12 +3,27 @@ import os
 import uuid
 import zmq
 import re
+import logging
 from tilota import settings
 from tilota.core.console import Console
 
 
+def logger():
+    logger = logging.getLogger('Utils')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.FileHandler(os.path.join(
+        os.path.dirname(__file__), 'utils.log'))
+    handler.setFormatter(logging.Formatter(
+        '%(asctime)s %(levelname)-8s %(name)-8s %(message)s'))
+    handler.setLevel(logging.DEBUG)
+    logger.addHandler(handler)
+    return logger
+
+
 def create_new_game(game_cmd):
+    logger().debug('Create new game with cmd : %s', game_cmd)
     tmp_ipc = 'ipc://%s/%s.ipc' % (settings.CACHE_PATH, str(uuid.uuid1()))
+    logger().debug('Connecting to %s', tmp_ipc)
     tmp_socket = zmq.Socket(zmq.Context(), zmq.PULL)
     tmp_socket.bind(tmp_ipc)
     daemon_socket = zmq.Socket(zmq.Context(), zmq.PUSH)
@@ -17,7 +32,9 @@ def create_new_game(game_cmd):
     daemon_socket.send_json(
         {'name': 'get_game_id', 'pid': game.process.pid, 'reply_ipc': tmp_ipc}
     )
+    logger().debug('Waiting for game id')
     game_id = tmp_socket.recv_json()['game_id']
+    logger().debug('Retreive game id = %s', str(game_id))
     daemon_socket.send_json({'name': 'save_checkpoints', 'reply_ipc': tmp_ipc})
     tmp_socket.recv_json()
     tmp_socket.close()
